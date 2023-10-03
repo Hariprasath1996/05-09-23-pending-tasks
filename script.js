b1346f2c61f3185af93a48a75cfcc4b0259d8840
Hariprasath1996 <hasathphy96@gmail.com>
2023-10-03 20:45:57 +0530

    here created six logical questions that shows in web page

---
+++ script.js
@@ -0,0 +1,4 @@
+// 11.You are given an array of numbers. Write a program that finds the largest even number
+// in the array and displays the result on a web page.
+// Given input:John,Alice,Bob,Zara,David
+
